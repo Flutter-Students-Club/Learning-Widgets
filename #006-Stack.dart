d5c5c87e5f0c07295802131ea0af0b244a6c6c0d
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-05-29 22:56:22 +0300

    Create #006-Stack.dart

---
+++ #006-Stack.dart
@@ -0,0 +1,66 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #006 - Stack 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #006 - Stack 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Stack'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Stack(
+          children: [
+            Container(width: 200, height: 200, color: Colors.blue,),
+            Container(width: 150, height: 150, color: Colors.lightBlueAccent,),
+            Container(width: 100, height: 100, color: Colors.white,),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/beta-class-online
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
