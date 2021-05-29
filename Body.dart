1fac429d28a845f4b1b8ada1b6e0b41f47ddb014
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-05-29 22:49:34 +0300

    Create Body.dart

---
+++ Body.dart
@@ -0,0 +1,71 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #001 - Body 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #001 - Body💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Body'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                "Body kısmına resimler, yazılar ve birçok widget eklenebilir."),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: "Increment",
+        child: Icon(Icons.home),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/beta-class-online
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
