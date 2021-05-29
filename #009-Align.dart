7f6aa77322039933e8c4fa9968ee7c01e553a89e
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-05-29 23:00:46 +0300

    Create #009-Align.dart

---
+++ #009-Align.dart
@@ -0,0 +1,74 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #009 - Align 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #009 - Align 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Align'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Align(
+        alignment: Alignment.bottomRight,
+        child: Container(
+          color: Colors.lightBlueAccent,
+          width: 250,
+          height: 250,
+          child: Center(
+            child: Text(
+              "FlutterStudentsClub",
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/beta-class-online
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
