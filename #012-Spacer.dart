a208b5d86d1d3eb5cc1a0ba56cef9ff4dc4739c0
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-05-31 20:31:18 +0300

    Create #012-Spacer.dart

---
+++ #012-Spacer.dart
@@ -0,0 +1,118 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #012 - Spacer 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #012 - Spacer 💙
+*/
+
+// ÖRNEK 1
+// EXAMPLE 1
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Spacer'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(width: 150, height: 150, color: Colors.lightBlueAccent,),
+            Spacer(),
+            Container(width: 150, height: 150, color: Colors.blue,),
+            Container(width: 150, height: 150, color: Colors.lightBlueAccent,),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// ÖRNEK 2
+// EXAMPLE 2
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Spacer'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(width: 150, height: 150, color: Colors.lightBlueAccent,),
+            Spacer(flex: 80),
+            Container(width: 150, height: 150, color: Colors.blue,),
+            Spacer(flex: 20),
+            Container(width: 150, height: 150, color: Colors.lightBlueAccent,),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/beta-class-online
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
