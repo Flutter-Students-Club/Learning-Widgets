27b07e61a2c0721ef9e5edf4325a78d0e6df5969
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-05-31 20:32:46 +0300

    Create #013-DefaultTextStyle.dart

---
+++ #013-DefaultTextStyle.dart
@@ -0,0 +1,76 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #013 - DefaultTextStyle 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #013 - DefaultTextStyle 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'DefaultTextStyle'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DefaultTextStyle(
+              child: Text(
+                "Flutter Students Club",
+              ),
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 30,
+                decoration: TextDecoration.underline,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/beta-class-online
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
