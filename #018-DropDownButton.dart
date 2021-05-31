f4061d253d0282dc1cc1168fceda2d8d70e4a4df
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-05-31 20:39:58 +0300

    Create #018-DropDownButton.dart

---
+++ #018-DropDownButton.dart
@@ -0,0 +1,85 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #018 - DropDownButton 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #018 - DropDownButton 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'DropDown Button'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String? pizzaCesidi;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButton(
+              value: pizzaCesidi,
+              items: [
+                "Karışık Pizza",
+                "Pastırmalı Pizza",
+                "Mexico Pizza",
+                "İtalyan Pizza",
+                "Anadolu Pizza",
+                "Vejeteryan Pizza",
+              ].map((String label) => DropdownMenuItem(
+              child: Text(label),
+                value: label,
+              ))
+              .toList(),
+              onChanged: (String? value) {
+                setState(() {
+                  pizzaCesidi = value;
+                });
+              }, 
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/beta-class-online
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
