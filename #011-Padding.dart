99cf36fe03de3a5b033a0b756c95d0480b0560a5
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-05-31 20:27:52 +0300

    Create #011-Padding.dart

---
+++ #011-Padding.dart
@@ -0,0 +1,69 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #001 - Padding 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #001 - Padding 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Padding'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 20.0,),
+              child: Container(width: 150, height: 150, color: Colors.lightBlueAccent,),
+            ),
+            Container(width: 150, height: 150, color: Colors.blue,),
+            Container(width: 150, height: 150, color: Colors.lightBlueAccent,),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/beta-class-online
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
