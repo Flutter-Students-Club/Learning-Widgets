be3007ecf4bb1116b0ebe48cabd011b079c5daa5
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-06-09 12:06:53 +0300

    Create #022-Slider.dart

---
+++ #022-Slider.dart
@@ -0,0 +1,81 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #022 - Slider 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #022 - Slider 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Slider'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double ilerleme = 100;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Slider(
+                max: 100,
+                min: 0,
+                value: ilerleme,
+                activeColor: Colors.blue,
+                inactiveColor: Colors.red,
+                onChanged: (double i) {
+                  setState(() {
+                    ilerleme = i;
+                  });
+                },
+              ),
+              Text("Slider Değer: $ilerleme")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/flutter-students-club
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
