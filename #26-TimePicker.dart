23fde5d5cc7f67cd355efdaf3271dfbad8552784
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-06-09 12:13:32 +0300

    Create #26-TimePicker.dart

---
+++ #26-TimePicker.dart
@@ -0,0 +1,80 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #026 - TimePicker 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #026 - TimePicker 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Time Picker'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var tfTime = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+                controller: tfTime,
+                decoration: InputDecoration(
+                  hintText: "Saat giriniz",
+                ),
+                onTap: () {
+                  showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay.fromDateTime(DateTime.now()),
+                  ).then((dynamic timeData) {
+                    setState(() {
+                      tfTime.text = "${timeData.hour} : ${timeData.minute}";
+                    });
+                  });
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/flutter-students-club
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
