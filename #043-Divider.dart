2ad93614d376d987bc21b55fec112c1fb5a22032
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-07-23 15:30:29 +0300

    Create #043-Divider.dart

---
+++ #043-Divider.dart
@@ -0,0 +1,79 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #043 - Divider 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #043 - Divider 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Divider'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.red,
+              height: 100,
+            ),
+            Divider(
+              thickness: 5,
+              indent: 20,
+              endIndent: 20,
+              color: Colors.blue,
+              height: 50,
+            ),
+            Container(
+              color: Colors.orange,
+              height: 100,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/flutter-students-club
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
