b4230f60ddc46796d90912259d5990f4753780a0
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-07-23 15:35:57 +0300

    Create #049-GridView.dart

---
+++ #049-GridView.dart
@@ -0,0 +1,96 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #049 - GridView 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #049 - GridView 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'GridView'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final meyveler = [
+    "Elma",
+    "Kiraz",
+    "Şeftali",
+    "Armut",
+    "Mandalina",
+    "Portakal",
+    "Kivi",
+    "Üzüm",
+    "Muz",
+    "Çilek",
+    "Vişne",
+    "Ayva",
+    "Erik",
+    "Çağla",
+    "Kayısı",
+    "Dut",
+    "Yaban Mersini"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: GridView.builder(
+        itemCount: meyveler.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 15.0,
+          mainAxisSpacing: 10.0,
+        ),
+        itemBuilder: (context, index) {
+          return Container(
+            color: Colors.grey[300],
+            child: Center(
+              child: Text(
+                meyveler[index],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/flutter-students-club
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
