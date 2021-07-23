7e56e37a6ae1df100958c0715cb0c25a94eaa5d8
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-07-23 15:38:51 +0300

    Create #053-PageView.dart

---
+++ #053-PageView.dart
@@ -0,0 +1,68 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #053 - PageView 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #053 - PageView 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'PageView'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final PageController controller = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: PageView(
+        scrollDirection: Axis.horizontal,
+        controller: controller,
+        children: [
+          Center(child: Text("First Page")),
+          Center(child: Text("Second Page")),
+          Center(child: Text("Third Page")),
+        ],
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/flutter-students-club
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
