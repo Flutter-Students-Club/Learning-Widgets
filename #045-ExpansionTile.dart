bcb75debeebc0066ddb8074443c261f1c6c0a5da
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-07-23 15:32:19 +0300

    Create #045-ExpansionTile.dart

---
+++ #045-ExpansionTile.dart
@@ -0,0 +1,103 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #045 - ExpansionTile 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #045 - ExpansionTile 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'ExpansionTile'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            ExpansionTile(
+              title: Text(
+                "TITLE",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+              leading: Icon(
+                Icons.favorite,
+              ),
+              children: [
+                ExpansionTile(
+                  title: Text(
+                    "Sub title",
+                    style: TextStyle(
+                      color: Colors.red,
+                    ),
+                  ),
+                  leading: Icon(Icons.android),
+                  children: [
+                    ListTile(
+                      title: Text(
+                        "data",
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        "data",
+                        style: TextStyle(color: Colors.green),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/flutter-students-club
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
