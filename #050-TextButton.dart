622f1edf711ca671f2b663cfe261621ab1b30e8c
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-07-23 15:36:46 +0300

    Create #050-TextButton.dart

---
+++ #050-TextButton.dart
@@ -0,0 +1,75 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #050 - TextButton 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #050 - TextButton 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'TextButton'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Elma",
+              style: TextStyle(fontSize: 20),
+            ),
+            TextButton.icon(
+              label: Text("Sepete Ekle"),
+              icon: Icon(Icons.add),
+              onPressed: () {
+                print("Ürün sepete eklendi.");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/flutter-students-club
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
