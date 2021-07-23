86d917dbfd6f0694be6e36ec65a7f387e4819593
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-07-23 15:39:56 +0300

    Create #054-Positioned.dart

---
+++ #054-Positioned.dart
@@ -0,0 +1,76 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #054 - Positioned 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #054 - Positioned 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Positioned'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.blue[50],
+          height: 300,
+          child: Stack(
+            children: [
+              Positioned(
+                left: 10,
+                top: 30,
+                right: 50,
+                bottom: 70,
+                child: Container(
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/flutter-students-club
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
