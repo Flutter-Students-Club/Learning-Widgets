cfac8cc47cfeb3fcb103a8a60a17aa826f2350c0
emirhanerbay <76837844+emirhanerbay@users.noreply.github.com>
2021-07-23 15:34:19 +0300

    Create #047-ToolTip.dart

---
+++ #047-ToolTip.dart
@@ -0,0 +1,75 @@
+/* 
+-TR-
+Flutter Students Club 💙
+Widget öğreniyorum #047 - ToolTip 💙
+-EN-
+Flutter Students Club 💙
+Learning widget #047 - ToolTip 💙
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'ToolTip'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Tooltip(
+                message: "My Account",
+                showDuration: Duration(seconds: 5),
+                child: ElevatedButton(
+                  child: Icon(Icons.account_circle_sharp, size: 50),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Instagram : instagram.com/flutterstudentsclub
+Twitter : twitter.com/Flutterstudents
+LinkedIn : linkedin.com/company/flutterstudentsclub/
+Medium : medium.com/flutter-students-club
+Website : flutterstudentsclub.com
+----------------------------------------------------
+Flutter and the related logo are trademarks of Google LCC. Flutter  Students Club  isn’t affiliated with or otherwise sponsored by Google LLC.
+*/
